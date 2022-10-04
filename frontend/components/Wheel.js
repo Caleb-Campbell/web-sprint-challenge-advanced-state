00de05deaa0d12a51e781e6ff1001f8a12317673
Caleb-Campbell <calebcampbellcrm@gmail.com>
2022-10-04 10:12:34 -0600

    Actions down

--- frontend/components/Wheel.js
+++ frontend/components/Wheel.js
@@ -1,24 +1,42 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import {moveCounterClockwise, moveClockwise} from '../state/action-creators'
+import React, {useEffect} from 'react'
+import { connect } from 'react-redux'
+import { moveClockwise, moveCounterClocksise } from '../state/action-creators'
+import * as actionCreators from '../state/action-creators'
 
-function Wheel(props) {
+
+export function Wheel(props) {
+    
+
+  useEffect(() => {
+      console.log('Wheel Rendered')
+    },[])
+
+  // const handleClickRight = () => {
+  //   props.moveClockwise()
+  // }
+
+  // const handleClickLeft = () => {
+  //   props.moveCounterClockwise()
+  // }
+
   return (
     <div id="wrapper">
       <div id="wheel">
-        <div className="cog active" style={{ "--i": 0 }}>B</div>
-        <div className="cog" style={{ "--i": 1 }}></div>
-        <div className="cog" style={{ "--i": 2 }}></div>
-        <div className="cog" style={{ "--i": 3 }}></div>
-        <div className="cog" style={{ "--i": 4 }}></div>
-        <div className="cog" style={{ "--i": 5 }}></div>{/* --i is a custom CSS property, no need to touch that nor the style object */}
+        <div className={`cog${props.wheel === 0 ? ' active' : ''}`} style={{ "--i": 0 }}>{props.wheel === 0 ? 'B' : null}</div>
+        <div className={`cog${props.wheel === 1 ? ' active' : ''}`} style={{ "--i": 1 }}>{props.wheel === 1 ? 'B' : null}</div>
+        <div className={`cog${props.wheel === 2 ? ' active' : ''}`} style={{ "--i": 2 }}>{props.wheel === 2 ? 'B' : null}</div>
+        <div className={`cog${props.wheel === 3 ? ' active' : ''}`} style={{ "--i": 3 }}>{props.wheel === 3 ? 'B' : null}</div>
+        <div className={`cog${props.wheel === 4 ? ' active' : ''}`} style={{ "--i": 4 }}>{props.wheel === 4 ? 'B' : null}</div>
+        <div className={`cog${props.wheel === 5 ? ' active' : ''}`} style={{ "--i": 5 }}>{props.wheel === 5 ? 'B' : null}</div>
       </div>
       <div id="keypad">
-        <button onClick={props.moveCounterClockwise} id="counterClockwiseBtn" >Counter clockwise</button>
-        <button id="clockwiseBtn">Clockwise</button>
+        <button id="counterClockwiseBtn" onClick={props.moveCounterClockwise}>Counter clockwise</button>
+        <button id="clockwiseBtn" onClick={props.moveClockwise}>Clockwise</button>
       </div>
     </div>
   )
 }
 
-export default connect(state => state, {moveCounterClockwise, moveClockwise})(Wheel)
+
+
+export default connect(st => st, actionCreators)(Wheel)
--- frontend/state/action-creators.js
+++ frontend/state/action-creators.js
@@ -1,60 +1,110 @@
+import axios from 'axios'
+import * as types from './action-types'
+
+
 // ❗ You don't need to add extra action creators to achieve MVP
-const MOVE_CLOCKWISE = 'MOVE_CLOCKWISE'
-const MOVE_COUNTER_CLOCKWISE = 'MOVE__COUNTER_CLOCKWISE'
-const SELECT_ANSWER = 'SELECT_ANSWER'
-const SET_MESSAGE = 'SET_MESSAGE'
-const SET_QUIZ = 'SET_QUIZ'
-const INPUT_CHANGE = 'INPUT_CHANGE'
-const RESET_FORM = 'RESET_FORM'
-const FETCH_QUIZ = 'FETCH_QUIZ'
-const POST_ANSWER = 'FETCH_QUIZ'
-const POST_QUIZ = 'FETCH_QUIZ'
+export function moveClockwise() {  
+  return { 
+    type: types.MOVE_CLOCKWISE
+   } 
+}
 
-
-
-export function moveClockwise() { 
-  return {
-    type: type.MOVE_CLOCKWISE,}
+export function moveCounterClockwise() {
+  return { 
+    type: types.MOVE_COUNTERCLOCKWISE
+   }
  }
 
-export function moveCounterClockwise() { 
-  return {
-    type: type.MOVE_COUNTER_CLOCKWISE,
-  }
- }
+export function selectAnswer(num) { 
+  return { 
+    type: types.SET_SELECTED_ANSWER,
+     payload: num 
+    }
+}
 
-export function selectAnswer() { 
-  return{
-    type: SELECT_ANSWER
-  }
- }
-
-export function setMessage() { }
-
-export function setQuiz() { }
-
-export function inputChange() { }
-
-export function resetForm() { }
-
-// ❗ Async action creators
-export function fetchQuiz() {
+export function setMessage (message){ 
   return function (dispatch) {
-    // First, dispatch an action to reset the quiz state (so the "Loading next quiz..." message can display)
-    // On successful GET:
-    // - Dispatch an action to send the obtained quiz to its state
+    return { 
+      type: SET_INFO_MESSAGE,
+       payload: message 
+      }
   }
 }
-export function postAnswer() {
+
+export function setQuiz(quiz) {
+    return { 
+      type: types.SET_QUIZ_INTO_STATE,
+       payload: quiz  
+      }
+ }
+
+export function inputChange({name, value}) { 
+return { 
+  type: types.INPUT_CHANGE,
+   payload: {name, value} 
+  }
+}
+
+
+export function resetForm() {
+  return { type: types.RESET_FORM }
+ }
+
+export function fetchQuiz() {
+  const URL = 'http://localhost:9000/api/quiz/next'
   return function (dispatch) {
+    axios.get(URL)
+    .then((res) => {
+      dispatch({
+         type: types.SET_QUIZ_INTO_STATE,
+         payload: res.data})
+    }).catch((err) => { console.log({err})})
+  }
+}
+
+export function postAnswer(quizID, answerID) {
+ const URL = 'http://localhost:9000/api/quiz/answer'
+  return function (dispatch) {
+    axios.post(URL, { "quiz_id" : `${quizID}`, "answer_id": `${answerID}` })
+    .then((res) => {
+    dispatch({
+      type: types.SET_SELECTED_ANSWER,
+       payload: null
+      })
+    dispatch({
+      type: types.SET_INFO_MESSAGE,
+       payload: res.data.message
+      })
+    dispatch(fetchQuiz())
+    }).catch((err) => {
+      console.error({err})
+    })
+
     // On successful POST:
     // - Dispatch an action to reset the selected answer state
     // - Dispatch an action to set the server message to state
     // - Dispatch the fetching of the next quiz
   }
 }
-export function postQuiz() {
+export function postQuiz(newQuestion, newTrueAnswer, newFalseAnswer) {
+  const URL = 'http://localhost:9000/api/quiz/new'
   return function (dispatch) {
+    axios.post(URL, { "question_text": `${newQuestion}`, "true_answer_text": `${newTrueAnswer}`, "false_answer_text": `${newFalseAnswer}` })
+      .then(() => {
+        dispatch({
+          type: types.SET_INFO_MESSAGE,
+           payload: `Congrats: "${newQuestion}" is a great question!`
+          })
+        dispatch({
+          type: types.RESET_FORM
+        })
+      })
+      .catch((err) => {
+        ({
+          type: types.SET_INFO_MESSAGE,
+           payload: err.message
+          })
+      })
     // On successful POST:
     // - Dispatch the correct message to the the appropriate state
     // - Dispatch the resetting of the form
